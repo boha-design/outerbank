ba1388e7db67a3161b408f1374891e2653a3ff01
Sarah Basset <boha.design@outlook.fr>
2023-05-08 16:26:54 +0200

    ajout de responsive sur la modale en css et en js

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const imgIndex = document.querySelector('.bloc-modale img');
 
 //Création de l'événement click sur tous les btns
 
-btns.forEach(btn => { //forEach pour chaque btns
-    btn.addEventListener('click', (e) => {
-        imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
-        //e.target correspond au bouton sur lequel on vient de cliquer
-        //son attribut data-index correspond au numéro de l'image à récupérer
-        //cette dernière ligne renvoi au nom de l'image à chercher dans le dossier ressource
-        modaleEquipement.classList.add('active-modale');
-        //une fois que l'on a cliquer sur l'image, il faut faire apparaître la modale, on lui donne alors la class active-modale
+if(windows.matchMedia("(min-width: 850px)").matches){ //dit qu'à partir de 850px on ne veut plus de modal
+    btns.forEach(btn => { //forEach pour chaque btns
+        btn.addEventListener('click', (e) => {
+            imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
+            //e.target correspond au bouton sur lequel on vient de cliquer
+            //son attribut data-index correspond au numéro de l'image à récupérer
+            //cette dernière ligne renvoi au nom de l'image à chercher dans le dossier ressource
+            modaleEquipement.classList.add('active-modale');
+            //une fois que l'on a cliquer sur l'image, il faut faire apparaître la modale, on lui donne alors la class active-modale
+        })
     })
-})
-
-modaleEquipement.addEventListener('click', () => {
-    modaleEquipement.classList.remove('active-modale');
-}) // permet de fermer la modale en cliquant n'importe où, on garder quand même le fermeture-modale visuellement pour indiquer que l'on peut fermer
+    
+    modaleEquipement.addEventListener('click', () => {
+        modaleEquipement.classList.remove('active-modale');
+    }) // permet de fermer la modale en cliquant n'importe où, on garder quand même le fermeture-modale visuellement pour indiquer que l'on peut fermer
+//matchMedia est une fonction qui retourne true ou false et attend des arguements
+}
